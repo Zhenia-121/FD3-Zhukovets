8e09db12e15180a5a392c00436db1ebedae06852
Евегений Жуковец <jenia-121@mail.ru>
2019-04-22 21:33:08 +0300

    Added Projects

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.arrayOf(PropTypes.string).isRequired
+  };
+  
+  render() {
+    let frames = <p>{this.props.children}</p>;
+    let Width = "250px";
+    let Height = "150px";
+    let stepWH = "10px";
+    console.log(this.props.colors);
+    this.props.colors.forEach(color => {
+      let newFrame = <div style={{border:"solid 8px "+color,margin:"10px",height:Height,width:Width}}>{frames}</div>
+      frames = newFrame;
+      Width += stepWH;
+      Height += stepWH;
+    });
+    return (
+      <div className="rainbowFrame">
+        {frames}
+      </div>
+    );
+  }
+
+}
+
+export default RainbowFrame;
---
+++ br2jsx/components/br2jsx.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import './br2jsx.css';
+
+class BR2JSX extends React.Component {
+
+  static propTypes = {
+    text: PropTypes.string.isRequired
+  };
+  
+  render() {
+    let content = [];
+    let strings = this.props.text.split(/<br>|<br\/>|<br\s\/>/gm);
+    strings.forEach((str, index) => {
+      content.push(str, <br key={index}/>);  
+    });
+    return (
+      <div className="br2jsx">
+        {content}
+      </div>
+    );
+  }
+
+}
+
+export default BR2JSX;
